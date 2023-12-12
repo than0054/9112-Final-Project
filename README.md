9831b82071462651d1b5d1370276f7f0c163f213
than0054 <than0054@algonquinlive.com>
2023-12-12 14:55:20 -0500

    final

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1>Nadirsha_Thannickal_Kareem_Final_Project</>
+
+<script src="./getdaysuntilchristmas.js"></script>
+
+<code src="./getdaysuntilchristmas.js">
+
+
---
+++ getdaysuntilchristmas.js
@@ -0,0 +1,35 @@
+
+
+console.log("asdfghjkl");
+
+function remaining_days(days) {
+    let setDate = 25/12/2025;
+    // let today = getDate();
+    
+
+remaining_days();
+
+}
+// -----------------------------------
+
+function average() {
+    a = [1, 2, 3, 23, 5];
+    let total;
+    // total = a[1];
+    // console.log(total);
+    for(i=0; i <= 5; i++) {
+        total = a[i] + total;
+        // average = total / 2;
+        return total;
+
+    }
+    console.log(a[i]);
+    console.log(total);
+
+    average = total / 2 ;
+
+    console.log(average);
+
+}
+
+average();
